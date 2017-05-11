4a26739ee9d980e3fefba5283eb7bc078fe9b570
martinm1 <martin.mejzlik.1@gmail.com>
2017-05-11 17:41:55 +0200

    properties

--- src/main/java/cz/muni/fi/pv168/Main.java
+++ src/main/java/cz/muni/fi/pv168/Main.java
@@ -5,6 +5,8 @@
  */
 package cz.muni.fi.pv168;
 
+import java.io.IOException;
+import java.io.InputStream;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 import java.util.List;
+import java.util.Properties;
 import org.apache.derby.jdbc.ClientDriver;
 
 /**
@@ -23,10 +26,21 @@ public class Main {
     final static Logger log = LoggerFactory.getLogger(Main.class);
     
     public static DataSource createMemoryDatabase() {
+        
+        Properties conf = new Properties();
+        InputStream inputStream = Main.class.getResourceAsStream("/DBconfig.properties");
+        try {
+            conf.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }  
+        
+        
+        
         BasicDataSource bds = new BasicDataSource();
         bds.setDriverClassName(ClientDriver.class.getName());
         
-        bds.setUrl("jdbc:derby://localhost:1527/MissionDB");
+        bds.setUrl(conf.getProperty("dbUrl")/*jdbc:derby://localhost:1527/MissionDB"*/);
         //bds.setUsername("");
         //bds.setPassword("");
         
